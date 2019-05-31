ebd91d68253aa8f527aba9cccd65e9a50a45999a
zittoooo <stellar7484@naver.com>
2019-05-31 15:00:36 +0900

    Update MainActivity.java

--- STGAN_earring/app/src/main/java/com/example/jiho/stgan_earring/MainActivity.java
+++ STGAN_earring/app/src/main/java/com/example/jiho/stgan_earring/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -20,9 +21,11 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 
+import static android.os.Environment.DIRECTORY_DCIM;
 
 
 public class MainActivity extends Activity implements View.OnClickListener {
@@ -32,11 +35,12 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
     private Uri mlmageCaptureUri;
     private ImageView iv_UserPhoto;
+    private ImageView iv_Earring_Photo;
     private int id_view;
-    private String absoultePath;
+    public String absoultePath;
 
     private DB_Manger dbmanger;
-
+    private String mode;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -45,11 +49,32 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
         dbmanger = new DB_Manger();
 
-        iv_UserPhoto = (ImageView)this.findViewById(R.id.user_image);
-        Button btn_agree = (Button) this.findViewById(R.id.btn_UploadPicture);
-        btn_agree.setOnClickListener(this);
-    }
+        iv_UserPhoto = (ImageView) this.findViewById(R.id.user_image);
+        iv_Earring_Photo = (ImageView) this.findViewById(R.id.earring_image);
 
+        Button btn_upload = (Button) this.findViewById(R.id.btn_UploadPicture);
+        btn_upload.setOnClickListener(this);
+
+        Button btn_upload_earring = (Button) this.findViewById(R.id.btn_UploadEarring);
+        btn_upload_earring.setOnClickListener(this);
+
+
+        Button newpage = (Button) findViewById(R.id.newActivity);
+        newpage.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(MainActivity.this, SubActivity.class);
+                        Bitmap sendBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.picture);
+
+                        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                        sendBitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+                        byte[] byteArray = stream.toByteArray();
+                        intent.putExtra("image",byteArray);
+                        startActivity(intent);
+
+                    }
+                });
+    }
 
     /*
      * 카메라에서 사진 촬영
@@ -59,7 +84,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
         //임시로 사용할 파일의 경로 생성
-        String url = "tmp_" + String.valueOf(System.currentTimeMillis()) + ".jpg";
+        String url = "tmp_" + String.valueOf(System.currentTimeMillis()) + ".png";
         mlmageCaptureUri = Uri.fromFile(new File
                 (Environment.getExternalStorageDirectory(), url));
 
@@ -70,6 +95,8 @@ public class MainActivity extends Activity implements View.OnClickListener {
     /*
      * 앨범에서 이미지 가져오기
      * */
+
+
     public void doTakeAlbumAction() {
         //앨범호출
         Intent intent = new Intent(Intent.ACTION_PICK);
@@ -77,6 +104,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
         startActivityForResult(intent, PICK_FROM_ALBUM);
 
     }
+
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -112,12 +140,17 @@ public class MainActivity extends Activity implements View.OnClickListener {
                 }
 
                 final Bundle extras = data.getExtras();
-                String filePath = Environment.getDownloadCacheDirectory().getAbsolutePath() +
-                        "/SmartWheel/" + System.currentTimeMillis() + ".jpg";
+                 String filePath =Environment.getExternalStorageDirectory().getAbsolutePath() +
+                         "/SmartWheel/" + System.currentTimeMillis() + ".png";
 
                 if (extras != null) {
                     Bitmap photo = extras.getParcelable("data");
-                    iv_UserPhoto.setImageBitmap(photo);
+
+                    if(mode.equals("user"))
+                      iv_UserPhoto.setImageBitmap(photo);
+                     else if(mode.equals("earring"))
+                       iv_Earring_Photo.setImageBitmap(photo);
+
 
                     storeCropImage(photo, filePath);
                     absoultePath = filePath;
@@ -139,25 +172,24 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
     private void storeCropImage(Bitmap bitmap, String filePath) {
         //smartWheel폴더를 생성하여 이미지를 저장하는 방식
-        String dirPath = Environment.getDownloadCacheDirectory().getAbsolutePath()+
-                "/SmartWheel";
+        String dirPath = Environment.getExternalStorageDirectory().getAbsolutePath()+
+                "/SmartWheel/";
 
         File directory_SmartWheel = new File(dirPath);
 
-        if(!directory_SmartWheel.exists())
-            directory_SmartWheel.mkdir();
+        if(!directory_SmartWheel.exists()) directory_SmartWheel.mkdir();
 
         File copyFile = new File(filePath);
         BufferedOutputStream out = null;
 
-        try{
+        try {
             copyFile.createNewFile();
             out = new BufferedOutputStream(new FileOutputStream(copyFile));
-            bitmap.compress(Bitmap.CompressFormat.JPEG,100,out);
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, out);
 
-            sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE,Uri.fromFile(copyFile)));
+            sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, Uri.fromFile(copyFile)));
 
-        } catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
@@ -168,13 +200,16 @@ public class MainActivity extends Activity implements View.OnClickListener {
     public void onClick(View v) {
 
         id_view = v.getId();
+
         if (v.getId() == R.id.btn_UploadPicture) {
+            mode="user";
             DialogInterface.OnClickListener cameraListener = new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialog, int which) {
                     doTakePhotoAction();
                 }
             };
+
             DialogInterface.OnClickListener albumListener = new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialog, int which) {
@@ -197,6 +232,41 @@ public class MainActivity extends Activity implements View.OnClickListener {
                     .show();
 
         }
-    }
 
+        if (v.getId() == R.id.btn_UploadEarring) {
+            mode="earring";
+            DialogInterface.OnClickListener cameraListener = new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    doTakePhotoAction();
+                }
+            };
+
+            DialogInterface.OnClickListener albumListener = new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    doTakeAlbumAction();
+                }
+
+            };
+
+            DialogInterface.OnClickListener cancelListener = new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                }
+            };
+
+            new AlertDialog.Builder(this).setTitle("업로드할 이미지 선택")
+                    .setPositiveButton("사진촬영", cameraListener)
+                    .setNeutralButton("앨범선택", albumListener)
+                    .setNegativeButton("취소", cancelListener)
+                    .show();
+
+        }
+
+
+
+
+    }
 }
